b43133572fff3727b19be2de441191b8ff764d67
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2014-05-24 00:13:08 -0300

    bind home & end

--- conf/bindkeys.zsh
+++ conf/bindkeys.zsh
@@ -4,11 +4,12 @@
 
 bindkey "^K"      kill-whole-line                      # ctrl-k
 bindkey "^R"      history-incremental-search-backward  # ctrl-r
-bindkey "^A"      beginning-of-line                    # ctrl-a  
-bindkey "^E"      end-of-line                          # ctrl-e
+bindkey "[[1"     beginning-of-line                    # ctrl-a  
+bindkey "[[4"      end-of-line                          # ctrl-e
 bindkey "[B"      history-search-forward               # down arrow
 bindkey "[A"      history-search-backward              # up arrow
 bindkey "^D"      delete-char                          # ctrl-d
 bindkey "^F"      forward-char                         # ctrl-f
 bindkey "^B"      backward-char                        # ctrl-b
-bindkey -e   # Default to standard emacs bindings, regardless of editor string
+bindkey -e                                             # Default to standard emacs bindings, regardless of editor string
+
